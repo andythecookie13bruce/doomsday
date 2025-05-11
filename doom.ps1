9c51f1da4f96bf937b6ab9075cea0efc99c693ce
andythecookie13bruce <andrey.andy2345@gmail.com>
2025-05-11 21:56:55 +0300

    Update doom.ps1

--- doom.ps1
+++ doom.ps1
@@ -83,7 +83,7 @@ $html = @"
     body {
       margin: 0;
       overflow: hidden;
-      background: url('https://raw.githubusercontent.com/andythecookie13bruce/doomsdaysday/main/doomsday.jpg') no-repeat center center fixed;
+      background: url('https://raw.githubusercontent.com/andythecookie13bruce/doomsday/main/doomsday.jpg') no-repeat center center fixed;
       background-size: cover;
       color: #ff0000;
       font-family: 'Creepster', 'UnifrakturCook', cursive;
